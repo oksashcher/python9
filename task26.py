ac0dfcf46aeb561a5fc163035ece9bae8634b806
Oksana <avalonv264@gmail.com>
2023-04-01 14:30:40 +0300

    task26.py

---
+++ task26.py
@@ -0,0 +1,27 @@
+#def power(a, b):
+#    if a == 0:
+#        return 1
+#    if a < 0:
+#        return 1 / power(b, -a)
+#    if a % 2 == 0:
+#        return power(b, a // 2) * power(b, a// 2)
+#    else:
+#        return power(b, a - 1) * x
+
+#b = int(input())
+#a = int(input())
+#print(power(b, a))
+
+
+
+
+#def summa(a, b):
+#    a += 1
+#    b -= 1
+#    if b > 0:
+#        return summa(a, b)
+#    else:
+#        return a
+ 
+ 
+#print sum(3, 5)
